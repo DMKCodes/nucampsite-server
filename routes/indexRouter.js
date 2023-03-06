b1f8760b39717617b9e672c24c648524725998e8
Doug <dmkcodes@gmail.com>
2023-03-06 13:34:41 -0500

    add https secure server config

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var indexRouter = express.Router();
+const express = require('express');
+const indexRouter = express.Router();
 
 /* GET home page. */
 indexRouter.get('/', function(req, res, next) {
